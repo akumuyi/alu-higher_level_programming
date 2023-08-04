5934bae53abeda5fd47c24663f86173a21181b18
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-08-04 00:46:17 -0700

    My privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Lists all privileges for user_0d_1
--- Lists all privileges for user_0d_2
+-- script to list privelages of users on server
+-- DML QUERY to do above
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
