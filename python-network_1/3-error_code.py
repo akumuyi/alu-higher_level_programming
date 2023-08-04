c19f743a853cc09a181291b7e8b7e5e689d9aa1c
a.kumuyi <a.kumuyi@alustudent.com>
2023-08-04 09:19:18 +0000

    Error code #0

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
+"""
+Python script to fetch and display the body of a URL response.
+"""
+
 import urllib.request
 import urllib.error
 import sys
 
-def fetch_url_content(url):
+def fetch_and_display_url_body(url):
+    """
+    Fetches the body of a URL response and displays it.
+
+    Args:
+        url (str): The URL to fetch the response from.
+
+    Returns:
+        str: The decoded body of the response.
+
+    Raises:
+        urllib.error.HTTPError: If an HTTP error occurs (e.g., 404, 500).
+    """
     try:
         with urllib.request.urlopen(url) as response:
-            print("Response body:")
-            content = response.read().decode('utf-8')
-            print(content)
+            body = response.read().decode('utf-8')
+            print("Response Body:")
+            print(body)
+            return body
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
-    except urllib.error.URLError as e:
-        print(f"Error occurred: {e.reason}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        return ""
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script_name.py <URL>")
+        print("Usage: python3 script_name.py <url>")
     else:
         url = sys.argv[1]
-        fetch_url_content(url)
+        fetch_and_display_url_body(url)
 
