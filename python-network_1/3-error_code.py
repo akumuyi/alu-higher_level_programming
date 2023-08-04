c7868a81a1b8f65a1155a0ebd061618edea40e64
a.kumuyi <a.kumuyi@alustudent.com>
2023-08-04 09:35:49 +0000

    Error code #0

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -3,6 +3,9 @@
 This module contains a Python script that takes in a URL, sends a request to the URL, and displays the body of the response.
 """
 
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
+
 import urllib.request
 import urllib.error
 import sys
