7a5827a451ccfadf63054b16d991269c75f1c7a8
a.kumuyi <a.kumuyi@alustudent.com>
2023-08-04 09:11:05 +0000

    Error code #0

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,24 @@
+import urllib.request
+import urllib.error
+import sys
+
+def fetch_url_content(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            print("Response body:")
+            content = response.read().decode('utf-8')
+            print(content)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+    except urllib.error.URLError as e:
+        print(f"Error occurred: {e.reason}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <URL>")
+    else:
+        url = sys.argv[1]
+        fetch_url_content(url)
+
