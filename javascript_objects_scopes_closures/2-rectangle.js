087394c748dd7782afe3cf206ea627ab7775ab4a
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-10-01 01:25:18 -0700

    Rectangle #2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      return {}; // Create an empty object if w or h is not positive
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
