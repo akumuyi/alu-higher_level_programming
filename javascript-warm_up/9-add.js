f8ada2c3b48d47d1c0185908df98a7b206863270
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-09-30 17:19:43 -0700

    Add

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-function add(a, b) {
-  return a + b;
-}
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
-if (!isNaN(arg1) && !isNaN(arg2)) {
-  const result = add(arg1, arg2);
-  console.log(result);
+const { argv } = require('process');
+const firstNumber = parseInt(argv[2]);
+const secondNumber = parseInt(argv[3]);
+if (isNaN(firstNumber)) {
+  console.log(firstNumber);
+} else if (isNaN(secondNumber)) {
+  console.log(secondNumber);
 } else {
-  console.log('Invalid input. Please provide two integers.');
+  add(firstNumber, secondNumber);
+}
+function add (a, b) {
+  console.log(a + b);
 }
