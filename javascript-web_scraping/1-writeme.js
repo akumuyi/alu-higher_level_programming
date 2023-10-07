8713247458d6dcded2acb61390df25e7f9d49ce5
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-10-07 02:52:18 -0700

    Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,22 +1,25 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const fs = require('fs/promises');
+const fs = require('fs');
 const writeFile = async (filePath, string) => {
-  const fd = await fs.open(filePath, 'w');
-  await fs.write(fd, string, 'utf-8');
-  await fs.close(fd);
+  process.emitWarning = (warning, ... args) => {
+    if (args[0] === 'ExperimentalWarning') {
+      return;
+    }
+    return process.emitWarning(warning, ... args);
+  };
+  try {
+    await fs.promises.writeFile(filePath, string, 'utf-8');
+  } catch (error) {
+    console.error(error);
+  }
 };
 const main = async () => {
-  const filePath = argv[2];
-  const string = argv[3];
+  const filePath = process.argv[2];
+  const string = process.argv[3];
   if (!filePath || !string) {
     console.error('Usage: 1-writeme.js <file_path> <string>');
     return;
   }
-  try {
-    await writeFile(filePath, string);
-  } catch (err) {
-    console.error(err);
-  }
+  await writeFile(filePath, string);
 };
 main();
