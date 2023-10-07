13c97fea500c1a2fb26b213ee26a122f9156614e
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-10-07 02:45:52 -0700

    Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,43 +1,25 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const fs = require('fs');
-const closeFd = (fd, callback) => {
-  fs.close(fd, (err) => {
-    if (err) {
-      callback(err);
-      return;
-    }
-    callback();
-  });
+const closeFd = async (fd) => {
+  await fs.promises.close(fd);
 };
-const writeFile = (filePath, string, callback) => {
-  fs.open(filePath, 'w', (err, fd) => {
-    if (err) {
-      callback(err);
-      return;
-    }
-    fs.write(fd, string, 'utf-8', (err, written, string) => {
-      if (err) {
-        callback(err);
-        return;
-      }
-      closeFd(fd, callback);
-    });
-  });
+const writeFile = async (filePath, string) => {
+  const fd = await fs.promises.open(filePath, 'w');
+  await fs.promises.write(fd, string, 'utf-8');
+  await closeFd(fd);
 };
-const main = () => {
+const main = async () => {
   const filePath = argv[2];
   const string = argv[3];
   if (!filePath || !string) {
     console.error('Usage: 1-writeme.js <file_path> <string>');
     return;
   }
-  writeFile(filePath, string, (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log('File written successfully!');
-  });
+  try {
+    await writeFile(filePath, string);
+  } catch (err) {
+    console.log(err);
+  }
 };
 main();
