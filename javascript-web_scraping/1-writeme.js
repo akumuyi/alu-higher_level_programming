483f4a43c52252cfbd827878d35c6fd50d082241
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-10-07 02:33:50 -0700

    Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 const fs = require('fs');
 const writeFile = async (filePath, string) => {
+  process.emitWarning = (warning, ... args) => {
+    if (args[0] === 'ExperimentalWarning') {
+      return;
+    }
+    return process.emitWarning(warning, ... args);
+  };
   try {
     await fs.promises.writeFile(filePath, string, 'utf-8');
   } catch (error) {
-    if (error.code === 'ENOENT') {
-      console.error('File does not exist:', filePath);
-    } else {
-      console.error(error);
-    }
+    console.error(error);
   }
 };
 const main = async () => {
