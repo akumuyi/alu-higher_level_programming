dbac43b4e9940d2712c6af0e8c1996fab09dbd25
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-10-07 00:52:33 -0700

    Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
 const writeFile = async (filePath, string) => {
+  process.emitWarning = (warning, ... args) => {
+    if (args[0] === 'ExperimentalWarning') {
+      return;
+    }
+    return process.emitWarning(warning, ... args);
+  };
   try {
     await fs.promises.writeFile(filePath, string, 'utf-8');
   } catch (error) {
