17a188676424901e9eae4be15856bce6cf8c44f4
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-10-07 00:31:53 -0700

    Readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,24 +1,26 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const readFile = async (filePath) => {
-  try {
-    const data = await fs.promises.readFile(filePath, 'utf-8');
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
+const readFile = (filePath, callback) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data);
+    }
+  });
 };
-
-const main = async () => {
+const main = () => {
   const filePath = process.argv[2];
   if (!filePath) {
     console.error('Usage: 0-readme.js <file_path>');
     return;
   }
-
-  await readFile(filePath);
+  readFile(filePath, (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
+    }
+  });
 };
-
 main();
-
