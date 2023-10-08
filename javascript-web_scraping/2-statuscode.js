e8a0cf9ddfd3f020b1329112f7ed0859ec212417
Abiodun Kumuyi <a.kumuyi@alustudent.com>
2023-10-08 08:20:49 -0700

    status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const { argv } = require('process');
-const main = async () => {
-  const url = argv[2];
-  if (!url) {
-    console.error('Usage: 2-statuscode.js <url>');
-    return;
-  }
-  const response = await request(url);
-  console.log(`code: ${response.statusCode}`);
+
+const options = {
+  method: 'GET',
+  uri: argv[2]
 };
-main();
+
+if (argv[2]) {
+  request(argv[2], options, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log(`code: ${response.statusCode}`);
+  });
+} else {
+  console.log('An error occured missing url command line arg.');
+}
