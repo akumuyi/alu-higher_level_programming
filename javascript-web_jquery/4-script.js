58a81267e671b73bb1508566ea5c586534eff973
akumuyi <a.kumuyi@alustudent.com>
2023-10-09 11:39:39 -0400

    Toggle classes

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,23 @@
+// Select the header element using jQuery
+const header = $('header');
+
+// Select the DIV#toggle_header element using jQuery
+const toggleHeaderDiv = $('#toggle_header');
+
+// Attach a click event listener to the DIV#toggle_header element
+toggleHeaderDiv.on('click', function() {
+  // Get the current class of the header element
+  const currentClass = header.attr('class');
+
+  // If the current class is red, remove the red class and add the green class
+  if (currentClass === 'red') {
+    header.removeClass('red');
+    header.addClass('green');
+  }
+
+  // Otherwise, if the current class is green, remove the green class and add the red class
+  else if (currentClass === 'green') {
+    header.removeClass('green');
+    header.addClass('red');
+  }
+});
