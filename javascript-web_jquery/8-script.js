452f66e7054967998318197de0e3ff656d40987d
akumuyi <a.kumuyi@alustudent.com>
2023-10-09 11:56:18 -0400

    Star Wars movies

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,13 @@
+// Make an ajax request to fetch the movie titles
+$.ajax({
+    url: "https://swapi-api.alx-tools.com/api/films/?format=json",
+    success: function(response) {
+      // Get the list of movie titles
+      const movieTitles = response.results.map(movie => movie.title);
+  
+      // Append each movie title to the UL#list_movies element
+      movieTitles.forEach(title => {
+        $('#list_movies').append($('<li>' + title + '</li>'));
+      });
+    }
+  });  
