f89021da36d511155b571bb806581b6b983281f1
akumuyi <a.kumuyi@alustudent.com>
2023-10-09 11:02:57 -0400

    Add `.red` class

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,11 @@
+// Select the header element using jQuery
+const header = $('header');
+
+// Select the DIV#red_header element using jQuery
+const redHeaderDiv = $('#red_header');
+
+// Attach a click event listener to the DIV#red_header element
+redHeaderDiv.on('click', function() {
+  // Add the class `red` to the header element
+  header.addClass('red');
+});
