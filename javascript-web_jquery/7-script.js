1b5f22638fb0d1a9a4781dc43761d50a0641eeb1
akumuyi <a.kumuyi@alustudent.com>
2023-10-09 11:52:43 -0400

    Star wars character

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // Send an HTTP GET request to the API URL
+    $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data) {
+      // Extract the character name from the response data
+      var characterName = data.name;
+      
+      // Update the content of the DIV#character with the character name
+      $('#character').text(characterName);
+    });
+  });  
