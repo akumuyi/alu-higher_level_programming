2a7315a5a979536e1809f98c12e91f44a0d8fba3
akumuyi <a.kumuyi@alustudent.com>
2023-10-09 11:47:00 -0400

    Change the text

---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,11 @@
+// Select the DIV#update_header element using jQuery
+const updateHeaderDiv = $('#update_header');
+
+// Select the header element using jQuery
+const header = $('header');
+
+// Attach a click event listener to the DIV#update_header element
+updateHeaderDiv.on('click', function() {
+  // Set the text of the header element to New Header!!!
+  header.text('New Header!!!');
+});
