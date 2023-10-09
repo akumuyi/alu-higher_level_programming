41a3a0345376f2c3d8f7148a8b49d4bbfa9f85f8
akumuyi <a.kumuyi@alustudent.com>
2023-10-09 11:43:29 -0400

    List of elements

---
+++ javascript-web_jquery/5-script.js
@@ -0,0 +1,14 @@
+// Select the DIV#add_item element using jQuery
+const addItemDiv = $('#add_item');
+
+// Select the UL.my_list element using jQuery
+const myListUl = $('.my_list');
+
+// Attach a click event listener to the DIV#add_item element
+addItemDiv.on('click', function() {
+  // Create a new `<li>` element
+  const newLiElement = $('<li>Item</li>');
+
+  // Append the new `<li>` element to the UL.my_list element
+  myListUl.append(newLiElement);
+});
